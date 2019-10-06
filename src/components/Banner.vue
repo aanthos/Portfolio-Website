bd637e6e6118fee14a296f8a2213e3516af3c90e
aanthos <anthos@gmail.com>
2019-10-06 17:06:18 -0600

    Added banner picture

---
+++ src/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="banner">
+        <!-- <img src="@/assets/sand-dunes.jpg"> -->
+
+    </div>
+</template>
+
+<style lang="less" scoped>
+#banner {
+    // height: calc(100vh - 94px);
+    height: calc(100vh);
+    background: url('../assets/sand-dunes-2.jpg');
+    background-size: cover;
+}    
+</style>
