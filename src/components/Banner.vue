0d58f2b333e5274f7507bf67d80c6138b6cdd274
aanthos <anthos@gmail.com>
2019-10-06 20:52:28 -0600

    Installed Vue-Awesome and added bottom arrow

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,15 +1,82 @@
 <template>
     <div id="banner">
-        <!-- <img src="@/assets/sand-dunes.jpg"> -->
-
+        <!-- <div id="banner-image"></div> -->
+        <img id="banner-image" src="../assets/sand-dunes-2.jpg">
+        <div id="background-cutout">
+            <v-icon name="angle-down" />
+        </div>
     </div>
 </template>
 
 <style lang="less" scoped>
+// #banner {
+//     // height: calc(100vh - 94px);
+//     height: calc(100vh);
+//     position: relative;
+//     line-height: 0px;
+//     &:before {
+//         right: 50%;
+//         border-right: 40px solid transparent;
+//         border-left: 1000px solid white;
+//     }
+//     &:after {
+//         left: 50%;
+//         border-left: 40px solid transparent;
+//         border-right: 1000px solid white;
+//     }
+//     &:before, &:after {
+//         content: '';
+//         position: absolute;
+//         bottom: 0;
+//         width: 50%;
+//         border-bottom: 40px solid white;
+//         transform: rotate(0.000001deg);
+//     }
+// }
+
 #banner {
-    // height: calc(100vh - 94px);
+    position: relative;
+    overflow: hidden;
+//   line-height: 0px;
     height: calc(100vh);
-    background: url('../assets/sand-dunes-2.jpg');
-    background-size: cover;
-}    
+}
+
+#banner:before {
+  right: 50%;
+  border-right: 40px solid transparent;
+  border-left: 1000px solid #fff;
+}
+
+#banner:after {
+  left: 50%;
+  border-left: 40px solid transparent;
+  border-right: 1000px solid #fff;
+}
+
+#banner:before, #banner:after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  width: 50%;
+  border-bottom: 40px solid #fff;
+//   -moz-transform: rotate(0.000001deg);
+//   -webkit-transform: rotate(0.000001deg);
+//   -o-transform: rotate(0.000001deg);
+//   -ms-transform: rotate(0.000001deg);
+//   transform: rotate(0.000001deg);
+}
+
+// #banner-image {
+//     height: 100%;
+//     width: 100%;
+//     overflow: hidden;
+//     position: relative;
+//     background: url('../assets/sand-dunes-2.jpg');
+//     background-size: cover;
+// }
+
+#banner-image {
+    max-width: 100%;
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './bootstrap.config';
+import './vue-awesome.config';
 
 Vue.config.productionTip = false;
 
